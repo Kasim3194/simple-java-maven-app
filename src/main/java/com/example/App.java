3843f634637061e9f31ec1fbb928509c1336dec6
kasim <kasimshaikhh98@gmail.com>
2025-07-27 22:28:00 +0530

    Update App.java

--- src/main/java/com/example/App.java
+++ src/main/java/com/example/App.java
@@ -1,13 +1,15 @@
 package com.example;
+
+import java.io.*;
+import javax.servlet.*;
 import javax.servlet.http.*;
 
-public class App {
-    public static void main(String[] args) {
-        System.out.println("Hello World!");
-    }
+public class HelloServlet extends HttpServlet {
+    protected void doGet(HttpServletRequest request, HttpServletResponse response)
+        throws ServletException, IOException {
 
-    public String getGreeting() {
-        return "Hello, World!";
-        
+        response.setContentType("text/html");
+        PrintWriter out = response.getWriter();
+        out.println("<h1>Hello from HelloServlet!</h1>");
     }
 }
